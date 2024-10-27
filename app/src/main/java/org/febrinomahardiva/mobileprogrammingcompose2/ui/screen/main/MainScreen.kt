1d96a2d9aeecd9d42c31b8faf14e85d5cd180e40
febrinomahardiva <febrinomahardiva@gmail.com>
2024-10-27 09:50:30 +0700

    3.3. Mengambil data kelas dari database

--- app/src/main/java/org/febrinomahardiva/mobileprogrammingcompose2/ui/screen/main/MainScreen.kt
+++ app/src/main/java/org/febrinomahardiva/mobileprogrammingcompose2/ui/screen/main/MainScreen.kt
@@ -1,13 +1,19 @@
 package org.febrinomahardiva.mobileprogrammingcompose2.ui.screen.main
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,7 +30,7 @@ import org.febrinomahardiva.mobpro2s.ui.UserProfileCard
 
 @Composable
 fun MainScreen(
-    user: FirebaseUser
+    user: FirebaseUser,
 ) {
     val factory = ViewModelFactory(user.uid)
     val viewModel: MainViewModel = viewModel(factory = factory)
@@ -43,18 +49,37 @@ fun MainScreen(
             }
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .padding(16.dp)
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding),
+            contentPadding = PaddingValues(bottom = 84.dp)
         ) {
-            UserProfileCard(user)
-        }
-        if (showDialog) {
-            KelasDialog(onDismissRequest = { showDialog = false }) {
-                viewModel.insert(it)
-                showDialog = false
+            item {
+                UserProfileCard(
+                    user = user,
+                    modifier = Modifier.padding(16.dp)
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
             }
+            items(viewModel.data) {
+                Card(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                ) {
+                    Text(
+                        text = it.nama,
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
+                    )
+                }
+            }
+        }
+    }
+
+    if (showDialog) {
+        KelasDialog(onDismissRequest = { showDialog = false }) {
+            viewModel.insert(it)
+            showDialog = false
         }
     }
 }

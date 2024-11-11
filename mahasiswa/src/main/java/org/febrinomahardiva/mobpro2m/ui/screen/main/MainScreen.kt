0170410c707b837f5f26363e0f3eb9d83d23e807
febrinomahardiva <febrinomahardiva@gmail.com>
2024-11-11 12:09:40 +0700

    3.1.Membuat dropdown kelas mahasiswa

--- mahasiswa/src/main/java/org/febrinomahardiva/mobpro2m/ui/screen/main/MainScreen.kt
+++ mahasiswa/src/main/java/org/febrinomahardiva/mobpro2m/ui/screen/main/MainScreen.kt
@@ -1,11 +1,41 @@
 package org.febrinomahardiva.mobpro2m.ui.screen.main
 
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.firebase.auth.FirebaseUser
 import org.febrinomahardiva.mobpro2m.R
 import org.febrinomahardiva.mobpro2s.ui.AppBarWithLogout
@@ -15,6 +45,8 @@ import org.febrinomahardiva.mobpro2s.ui.UserProfileCard
 fun MainScreen(
     user: FirebaseUser
 ) {
+    val viewModel: MainViewModel = viewModel()
+
     Scaffold(
         topBar = { AppBarWithLogout(R.string.app_name) }
     ) { innerPadding ->
@@ -24,6 +56,87 @@ fun MainScreen(
                 .padding(16.dp)
         ) {
             UserProfileCard(user)
+
+            PilihKelas(viewModel.dataKelas) {
+                Log.d("MainScreen", "Item terpilih: $it")
+            }
+        }
+    }
+}
+
+@Composable
+fun PilihKelas(
+    data: List<String>,
+    onConfirmation: (Int) -> Unit
+) {
+    var selectedItem by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.pilih_kelas),
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        DropDownKelas(data, selectedItem) {
+            selectedItem = it
+        }
+        Button(
+            onClick = { onConfirmation(selectedItem) },
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.simpan))
+        }
+    }
+}
+
+@Composable
+fun DropDownKelas(
+    data: List<String>,
+    selectedItem: Int,
+    onItemClick: (Int) -> Unit
+) {
+    var dropdownSize by remember { mutableStateOf(Size.Zero)}
+    var isExpanded by remember { mutableStateOf(false) }
+    Box {
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .onGloballyPositioned { dropdownSize = it.size.toSize() }
+                .border(
+                    border = BorderStroke(1.dp, Color.LightGray),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .clickable { isExpanded = true }
+        ) {
+            Text(
+                text = data[selectedItem],
+                modifier = Modifier.padding(16.dp)
+            )
+            Icon(
+                imageVector = Icons.Filled.ArrowDropDown,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
+        DropdownMenu(
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = false },
+            modifier = Modifier.width(with(LocalDensity.current) { dropdownSize.width.toDp() }
+            )
+        ) {
+            data.forEachIndexed { index, label ->
+                DropdownMenuItem(
+                    text = { Text(text = label) },
+                    onClick = {
+                        isExpanded = false
+                        onItemClick(index)
+                    }
+                )
+            }
         }
     }
 }
---
+++ mahasiswa/src/main/java/org/febrinomahardiva/mobpro2m/ui/screen/main/MainViewModel.kt
@@ -0,0 +1,11 @@
+package org.febrinomahardiva.mobpro2m.ui.screen.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val dataKelas = listOf(
+        "D3IF-46-02",
+        "D3IF-46-03",
+    )
+}

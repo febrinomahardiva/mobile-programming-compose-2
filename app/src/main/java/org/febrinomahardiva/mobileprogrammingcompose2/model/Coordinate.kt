1678bec4d142b4ac0eb479b49a2f4dd320608069
Ahmad Roji Muhyiddin <ahmadroji@student.telkomuniversity.ac.id>
2024-12-01 19:42:28 +0700

    3.6.Menambahkan marker di peta

--- app/src/main/java/org/febrinomahardiva/mobileprogrammingcompose2/model/Coordinate.kt
+++ app/src/main/java/org/febrinomahardiva/mobileprogrammingcompose2/model/Coordinate.kt
@@ -1,7 +1,9 @@
 package org.febrinomahardiva.mobileprogrammingcompose2.model
 
+import kotlin.math.atan
 import kotlin.math.cos
 import kotlin.math.ln
+import kotlin.math.sinh
 import kotlin.math.tan
 
 data class Coordinate(
@@ -11,4 +13,13 @@ data class Coordinate(
     val xScaled = (longitude + 180.0) / 360.0
     private val lRadian = Math.toRadians(latitude)
     val yScaled = (1.0 - ln(tan(lRadian) + 1 / cos(lRadian)) / Math.PI) / 2.0
+
+    companion object {
+        fun from(xScaled: Double, yScaled:Double): Coordinate {
+            val longitude = xScaled * 360.0 - 100.0
+            val lRadian = atan(sinh(Math.PI * (1 - 2 * yScaled)))
+            val latitude = Math.toDegrees(lRadian)
+            return Coordinate(latitude, longitude)
+        }
+    }
 }

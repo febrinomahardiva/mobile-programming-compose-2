2f510bb1bf9341171837f84ec86aa985eebc656e
Ahmad Roji Muhyiddin <ahmadroji@student.telkomuniversity.ac.id>
2024-12-01 19:13:38 +0700

    3.4.Mengambil data lokasi pengguna

--- app/src/main/java/org/febrinomahardiva/mobileprogrammingcompose2/ui/screen/MainScreen.kt
+++ app/src/main/java/org/febrinomahardiva/mobileprogrammingcompose2/ui/screen/MainScreen.kt
@@ -1,9 +1,13 @@
 package org.febrinomahardiva.mobileprogrammingcompose2.ui.screen
 
 import android.Manifest
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
@@ -20,6 +24,9 @@ fun MainScreen(
     val viewModel: MainViewModel = viewModel()
     val mapState = viewModel.mapState
 
+    val context = LocalContext.current
+    val userLocation by viewModel.getUserLocation(context).collectAsState(null)
+
     val locationPermissionState = rememberPermissionState(
         Manifest.permission.ACCESS_FINE_LOCATION
     )
@@ -30,6 +37,12 @@ fun MainScreen(
 
         if (!locationPermissionState.status.isGranted) {
             locationPermissionState.launchPermissionRequest()
+        }
+    }
+
+    LaunchedEffect(userLocation) {
+        userLocation?.let {
+            Log.d("MainScreen", "Location: ${it.latitude}, ${it.longitude}")
         }
     }
 
